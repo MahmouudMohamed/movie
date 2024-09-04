587a3a3bf304895e1e09fb4ce43ed6557cd41d62
Mahmoud Ouf <mahmoudouf504@gmail.com>
2024-09-04 19:06:23 +0300

    add utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie/utils/bloc_observer.dart';
+import 'package:movie/utils/my_theme_data.dart';
 
 import 'home_screen.dart';
 
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
 
@@ -13,6 +17,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: MyThemeData.lightTheme,
       debugShowCheckedModeBanner: false,
       initialRoute: HomeScreen.routeName,
 
